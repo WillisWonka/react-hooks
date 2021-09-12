671ea692b872d3411c5b851f11946de03824ab03
WillisWonka <williambarros4519@gmail.com>
2021-09-12 02:27:26 +0000

    Aula 05 - Hooks

--- hooks/src/components/layout/Content.jsx
+++ hooks/src/components/layout/Content.jsx
@@ -1,10 +1,10 @@
 import './Content.css'
 import React from "react"
 import { Switch, Route } from 'react-router-dom'
-import Home from '../../views/contents/Home'
-import Conteudo01 from '../../views/contents/Conteudo01'
-import Conteudo02 from '../../views/contents/Conteudo02'
-import Conteudo03 from '../../views/contents/Conteudo03'
+import Home from '../../views/contents/Hooks'
+import Conteudo01 from '../../views/contents/UseState'
+import Conteudo02 from '../../views/contents/UseEffect'
+import Conteudo03 from '../../views/contents/UseContext'
 import NaoEncontrado from '../../views/contents//NaoEncontrado'
 
 const Content = props => (
@@ -13,13 +13,13 @@ const Content = props => (
             <Route exact path="/">
                 <Home />
             </Route>
-            <Route path="/conteudo01/:usuario">
+            <Route path="/UseState">
                 <Conteudo01 />
             </Route>
-            <Route path="/conteudo02">
+            <Route path="/UseEffect">
                 <Conteudo02 />
             </Route>
-            <Route path="/conteudo03">
+            <Route path="/UseContext">
                 <Conteudo03 />
             </Route>
             <Route path="*">
